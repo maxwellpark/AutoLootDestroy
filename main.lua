403f1b54d9f253676f4f2d802e4e106d74eee277
Max <61809127+maxwellpark@users.noreply.github.com>
2022-04-02 16:32:38 +0100

    Debug mode and logging

--- main.lua
+++ main.lua
@@ -1,28 +1,44 @@
-local soulstoneId = 16893;
-local maxSoulstones = 10; -- Configurable
+local itemId = 16893; -- Configurable
+local maxItemCount = 10; -- Configurable
+local EVENT_NAME = "UNIT_INVENTORY_CHANGED";
+local DEBUG = true;
 local frame = Init();
 
 local function destroyItems()
-    local soulstoneCount = GetItemCount(soulstoneId);
-    local destroyCount = soulstoneCount - maxSoulstones;
-
-    if player:HasItem(soulstoneId) and soulstoneCount > maxSoulstones then
-        player:RemoveItem(soulstoneId, destroyCount);
+    local soulstoneCount = GetItemCount(itemId);
+    local destroyCount = soulstoneCount - maxItemCount;
+    if DEBUG then
+        print("Player has %s items", soulstoneCount);
+        print("Destroying %s items from bags", destroyCount);
+    end
+    -- Destroy the difference between current and max counts
+    if player:HasItem(itemId) and soulstoneCount > maxItemCount then
+        player:RemoveItem(itemId, destroyCount);
     end
 end
 
 local function eventHandler(self, event)
-    print("Destroying items");
+    if DEBUG then
+        print("Event handler called...");
+    end  
     destroyItems();
 end
 
 function Init()
+    if DEBUG then 
+        print("Initialising addon...");
+        print("Attempting to create frame and subscribe to event '%s'", EVENT_NAME);
+    end
+    -- Frame for subscribing to events 
     local newFrame = CreateFrame("FRAME", "AddonFrame");
     newFrame:RegisterEvent("ITEM_LOOTED_HANDLER");
-    newFrame:SetScript("UNIT_INVENTORY_CHANGED", eventHandler);
+    newFrame:SetScript(EVENT_NAME, eventHandler);
     return newFrame;
 end
 
 function Disable()
+    if DEBUG then
+        print("Disabling addon...");
+    end
    frame.UnregisterAllEvents();
 end
