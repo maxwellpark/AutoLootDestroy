1ec43c7f9beeb5f655a50e53d6503c2c952588f7
Max <61809127+maxwellpark@users.noreply.github.com>
2022-04-02 15:17:43 +0100

    Make vars local

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-soulstone_id = 16893;
-max_soulstones = 10; -- Configurable
-soulstone_count = GetItemCount(soulstone_id);
-destroy_count = soulstone_count - max_soulstones;
+local soulstone_id = 16893;
+local max_soulstones = 10; -- Configurable
+local soulstone_count = GetItemCount(soulstone_id);
+local destroy_count = soulstone_count - max_soulstones;
 
 if player:HasItem(soulstone_id) and soulstone_count > max_soulstones then
     player:RemoveItem(soulstone_id, destroy_count);
