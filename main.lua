783f5857d1739660434f4151354319392fb27d7b
Max <61809127+maxwellpark@users.noreply.github.com>
2022-04-18 13:31:10 +0100

    Listen on CHAT_MSG_LOOT and delay execution

--- main.lua
+++ main.lua
@@ -1,16 +1,18 @@
+Name, Realm = UnitName("player");
 local itemId = 6265; -- Configurable
 local maxItemCount = 10; -- Configurable
 -- Rarities range from poor (0) to heirloom (7)
 local itemRarities = {0, 1} -- Configurable
-local EVENT_NAME = "UNIT_INVENTORY_CHANGED";
 local DEBUG = true;
 local ADDON_NAME = "Auto Loot Destroy";
+local LOOT_EVENT_NAME = "CHAT_MSG_LOOT";
+local WAIT_TIME = 0.1;
 SLASH_ALD1 = "/ald";
--- To get the current version:
--- /run print((select(4, GetBuildInfo())));
+-- To get the current version: /run print((select(4, GetBuildInfo())));
 Log = {}; -- Log to SavedVariables
 Bag = {};
 Bag.__index = Bag;
+Bags = {};
 
 function Bag:create(name, bagId, invId, slotCount)
     local bag = {};
@@ -22,55 +24,53 @@ function Bag:create(name, bagId, invId, slotCount)
     return bag;
 end
 
+Inventory = {};
+Inventory.__index = Inventory;
+
+function Inventory:create(totalSlots, usedSlots, freeSlots)
+    local inventory = {};
+    inventory.totalSlots = totalSlots;
+    inventory.usedSlots = usedSlots;
+    inventory.freeSlots = freeSlots;
+    return inventory;
+end
+
 function DestroyItems()
     local itemCount = GetItemCount(itemId);
     print("Item count:", itemCount);
-    local bags = GetBags();
-    print("Bags:", bags);
-    local bagCount = #bags;
-    print("Bag count:", bagCount);
+    Bags = GetBags();
+    local bagCount = #Bags;
     if itemCount <= maxItemCount then
         print("Player has less items than the max count. Stopping deletion.", maxItemCount);
         return
     end
     local destroyCount = itemCount - maxItemCount;
     print("Destroy count:", destroyCount);
-    print("Looping through bags...");
+    local destroyCounter = destroyCount;
     for bagId = 1,bagCount + 1,1 do
-        print("------");
         print("Bag ID:", bagId);
-        print("Bag slot count:", bags[bagId].slotCount);
-        for slotId = 1,bags[bagId].slotCount,1 do
-            ClearCursor();
-            print("---");
-            print("Slot ID:", slotId);
-            local bagItemId = GetContainerItemID(bagId, slotId);
-            if bagItemId ~= nil then
-                print("Bag item ID:", bagItemId);
-                local itemLink = GetContainerItemLink(bagId, slotId);
-                print("Bag item link:", itemLink);
-                print("Bag item ID matches:", bagItemId == itemId);
-                local texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bagId, slotId);
-                -- local rarity = GetItemRarity(bagItiemId);
-                -- print("Item rarity:", rarity);
-                -- local destroyRarity = table.contains(itemRarities, rarity);
-                -- print("Rarity matches:", destroyRarity);
-                if bagItemId == itemId then
-                    print("Bag item qualifies for deletion. Bag ID: " ..  bagId .. ". Slot ID: " .. slotId);
-                    print("Item locked:", locked);
-                    PickupContainerItem(bagId, slotId);
-                    local hasItem = CursorHasItem();
-                    print("hasItem:", hasItem);
-                    if hasItem then
-                    -- if true then
-                        print("Deleting cursor item");
-                        DeleteCursorItem();
-                        itemCount = GetItemCount(itemId);
-                        print("New item count:", itemCount);
-                        if itemCount <= maxItemCount then
-                            print("Item count reached max. Stopping deletion.");
-                            print("Max count:", maxItemCount);
-                            return
+        if Bags[bagId] ~= nil then
+            print("Bag slot count:", Bags[bagId].slotCount);
+            for slotId = 1,Bags[bagId].slotCount,1 do
+                ClearCursor();
+                local bagItemId = GetContainerItemID(bagId, slotId);
+                if bagItemId ~= nil then
+                    print("Bag item ID:", bagItemId);
+                    local itemLink = GetContainerItemLink(bagId, slotId);
+                    print("Bag item link:", itemLink);
+                    if bagItemId == itemId then
+                        print("Bag item qualifies for deletion. Bag ID: " ..  bagId .. ". Slot ID: " .. slotId);
+                        PickupContainerItem(bagId, slotId);
+                        if CursorHasItem() then
+                            print("Deleting cursor item");
+                            DeleteCursorItem();
+                            itemCount = GetItemCount(itemId);
+                            destroyCounter = destroyCounter - 1;
+                            print("New destroy counter:", destroyCounter);
+                            if itemCount <= maxItemCount or destroyCounter <= 0 then
+                                print("Item count reached max. " .. maxItemCount .. " Stopping deletion.");
+                                return
+                            end
                         end
                     end
                 end
@@ -83,16 +83,28 @@ end
 
 function GetBags()
     local bags = {};
-    for bagId = 1,4,1 do
+    for bagId = 1,5,1 do
         local name = GetBagName(bagId);
-        -- Inventory ID - the bag's inventory ID used in functions like PutItemInBag(inventoryId)
-        local invId = ContainerIDToInventoryID(bagId);
-        local slotCount = GetContainerNumSlots(bagId);
-        print("Bag:", bagId, name, invId, slotCount);
-        local bag = Bag:create(name, bagId, invId, slotCount);
-        bags[bagId] = bag;
+        if name ~= nil then
+            -- Inventory ID: the bag's inventory ID used in functions like PutItemInBag(inventoryId)
+            local invId = ContainerIDToInventoryID(bagId);
+            local slotCount = GetContainerNumSlots(bagId);
+            local bag = Bag:create(name, bagId, invId, slotCount);
+            bags[bagId] = bag;
+        end
     end
     return bags;
+end
+
+function GetInventory(bags)
+    local totalSlots = 0;
+    local freeSlots = 0;
+    for bagId = 1,#Bags,1 do
+        totalSlots = totalSlots + bags[bagId].slotCount;
+        freeSlots = freeSlots + GetContainerNumFreeSlots(bagId);
+    end
+    local inventory = Inventory:create(totalSlots, totalSlots - freeSlots, freeSlots);
+    return inventory;
 end
 
 function GetItemRarity(id)
@@ -101,7 +113,6 @@ function GetItemRarity(id)
     return itemRarity;
 end
 
--- Type.method
 function table.contains(table, element)
     for _, e in ipairs(table) do
         if e == element then return true end
@@ -109,10 +120,19 @@ function table.contains(table, element)
     return false
 end
 
-local function eventHandler(self, event)
+function EventHandler(self, event)
     print("Event handler called...");
-    -- Hardware event
+    print("Event:", event);
+    if event and event.arg1 then
+        print("Event[1]:", event[5]);
+        print("Player name:", Name);
+    end
+    -- Hardware event for DestroyCursorItem()
     Button:Click();
+end
+
+function EventHandlerWait()
+    C_Timer.After(WAIT_TIME, EventHandler);
 end
 
 local function setSlashCmds()
@@ -123,12 +143,22 @@ local function setSlashCmds()
 end
 
 local function createOptions()
-    local panel = CreateFrame("Frame");
-    panel.name = ADDON_NAME;
-    InterfaceOptions_AddCategory(panel);
-    local title = panel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge");
-    title:SetPoint("TOP");
+    -- Container
+    local optionsFrame = CreateFrame("Frame", "ALD_OptionsFrame");
+    optionsFrame.name = ADDON_NAME;
+    InterfaceOptions_AddCategory(optionsFrame);
+    local title = optionsFrame:CreateFontString("ARTWORK", nil, "GameFontNormalLarge");
+    title:SetPoint("TOP", -8);
     title:SetText(ADDON_NAME);
+    -- Item ID edit box
+    -- local idEditBox = CreateFrame("EditBox", "ALD_ItemIdEditBox", optionsFrame);
+    -- InterfaceOptions_AddCategory(idEditBox);
+    -- idEditBox:SetPoint("LEFT", 16, 0);
+    -- idEditBox:SetPoint("RIGHT", -32, 0);
+    -- idEditBox:SetPoint("TOP", 0, -16);
+    -- idEditBox:SetPoint("BOTTOM", 0, 0);
+    -- Item name edit box
+
 end
 
 function Init()
@@ -136,12 +166,11 @@ function Init()
     print("Destroy item ID:", itemId);
     setSlashCmds();
     createOptions();
-    print("Attempting to create frame and subscribe to event", EVENT_NAME);
     -- Create frame for subscribing to events
-    local frame = CreateFrame("FRAME", "AddonFrame");
-    frame:RegisterEvent(EVENT_NAME);
-    frame:SetScript("OnEvent", eventHandler);
-    return frame;
+    local coreFrame = CreateFrame("FRAME", "ALD_CoreFrame");
+    coreFrame:RegisterEvent(LOOT_EVENT_NAME);
+    coreFrame:SetScript("OnEvent", EventHandlerWait);
+    return coreFrame;
 end
 
 local function clickHandler(self, event)
@@ -159,9 +188,9 @@ function Disable()
     if DEBUG then
         print("Disabling addon...");
     end
-   Frame.UnregisterAllEvents();
+   CoreFrame.UnregisterAllEvents();
 end
 
 -- Entry point
-Frame = Init();
+CoreFrame = Init();
 Button = CreateButton();
